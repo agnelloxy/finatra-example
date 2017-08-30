09b16efd947866c085a83cc529b3743dfee4295d
agnelloxy <agnelloxy@gmail.com>
2017-08-30 12:48:44 +0900

    return json

--- src/main/scala/com/github/agnelloxy/finatra/ApplicationController.scala
+++ src/main/scala/com/github/agnelloxy/finatra/ApplicationController.scala
@@ -3,8 +3,13 @@ package com.github.agnelloxy.finatra
 import com.twitter.finatra.http.Controller
 import com.twitter.finagle.http.Request
 
+case class ExampleCaseClass(id: Option[Int], name: String)
+
 class ApplicationController extends Controller {
   get("/") { request: Request =>
     "Hello, world!"
+  }
+  get("/foo") {request: Request =>
+    ExampleCaseClass(Some(1), "Example Name")
   }
 }
