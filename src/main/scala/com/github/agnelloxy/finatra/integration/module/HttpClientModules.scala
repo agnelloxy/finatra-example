b9fd7df0a8f2e8c4b60b1ac358fa16f99634b8b0
agnelloxy <agnelloxy@gmail.com>
2017-09-19 20:46:08 +0900

    refactor a bit

--- src/main/scala/com/github/agnelloxy/finatra/integration/module/HttpClientModules.scala
+++ src/main/scala/com/github/agnelloxy/finatra/integration/module/HttpClientModules.scala
@@ -14,6 +14,16 @@ object HttpClientModules {
 
 abstract class BasicHttpClientModule() extends TwitterModule {
 
+  protected def provideHttpClientFromClientName(mapper: FinatraObjectMapper, clientName: String, config: Config) = {
+    val path = "httpClient." + clientName
+    val specificConfig = config.getConfig(path)
+    provideHttpClient(mapper,
+      specificConfig.getString("host"),
+      specificConfig.getInt("port"),
+      specificConfig.getBoolean("ssl")
+    )
+  }
+
   protected def provideHttpClient(mapper: FinatraObjectMapper, host: String, port: Int = 80, ssl: Boolean = false): HttpClient = {
     val httpClientModule = new HttpClientModule {
       override def dest: String = s"$host:$port"
@@ -25,12 +35,11 @@ abstract class BasicHttpClientModule() extends TwitterModule {
 }
 
 object BlockChainHttpClientModule {
+
   def apply() = new BasicHttpClientModule {
     @Named("blockChain") @Provides @Singleton
-    def provideHttpClient(mapper: FinatraObjectMapper, config: Config) =
-      super.provideHttpClient(mapper,
-        config.getString("blockChain.host"),
-        config.getInt("blockChain.port"),
-        config.getBoolean("blockChain.ssl"))
+    def provideHttpClient(mapper: FinatraObjectMapper, config: Config) = {
+      super.provideHttpClientFromClientName(mapper, "blockChain", config)
+    }
   }
 }
